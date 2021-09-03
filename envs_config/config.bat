78694c92a16ebb9f7296bad06925b50df60be949
AiMiDi <Ai.Mi.Di@qq.com>
2021-09-03 20:44:54 +0800

    fix

--- envs_config/config.bat
+++ envs_config/config.bat
@@ -1,3 +1,4 @@
+conda install pyinstaller
 cd /d %~dp0
 activate vmdsizing_cython  && pip install cython  && pip install numpy  && pip install wxpython  && pip install numpy-quaternion  && pip install bezier  && pip install pypiwin32
-conda install pyinstaller
+
